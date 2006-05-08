9db1f191d9d7bd67f40e05351e5a7e954ffb0ec7
bearsfan <bearsfan@138b4e90-db10-0410-b6da-c669c309eabc>
2006-05-08 19:02:57 +0000

    Added support to extract the name from an algorithm and then appending it to the files produced

--- plugins/core/edu.iu.iv.core.rcp/src/edu/iu/iv/core/util/staticexecutable/StaticExecutableRunner.java
+++ plugins/core/edu.iu.iv.core.rcp/src/edu/iu/iv/core/util/staticexecutable/StaticExecutableRunner.java
@@ -22,7 +22,6 @@ import java.util.ResourceBundle;
 
 import org.eclipse.core.runtime.Platform;
 
-import edu.iu.iv.common.property.PropertyMap;
 import edu.iu.iv.core.IVC;
 import edu.iu.iv.core.UnsupportedModelException;
 import edu.iu.iv.core.datamodels.BasicDataModel;
@@ -30,23 +29,43 @@ import edu.iu.iv.core.datamodels.DataModel;
 import edu.iu.iv.core.datamodels.DataModelProperty;
 import edu.iu.iv.core.datamodels.DataModelType;
 import edu.iu.iv.core.persistence.BasicFileResourceDescriptor;
-import edu.iu.iv.core.persistence.FileResourceDescriptor;
 import edu.iu.iv.core.persistence.PersistenceException;
 import edu.iu.iv.core.persistence.PersistenceRegistry;
 import edu.iu.iv.core.persistence.Persister;
 
+/**
+ * Drops to the command line to execute a native program
+ * @author Team NWB
+ */
 public class StaticExecutableRunner {
-    String arguments;
-    File[] extraFiles;
-    String tempDirectory;
-    Map    fileToInfoMap;
+    private String    arguments;
+    private File[]    extraFiles;
+    private String    tempDirectory;
+    private Map       fileToInfoMap;
+    
+    //To set the hierarchy in the data model viewer
+    private DataModel parentDataModel;
+    private String    parentFilename;
 
+    /**
+     * Initialize class level variables
+     *
+     */
     public StaticExecutableRunner() {
-        tempDirectory = makeTempDirectory();
-        fileToInfoMap = new HashMap();
+        tempDirectory   = makeTempDirectory();
+        fileToInfoMap   = new HashMap();
+        parentDataModel = null;
+        parentFilename  = null;
     }
-        
+
+
+    /**
+     * Execute the plugin by copying the application into a temporary directory
+     * and then executing it
+     * @param pluginID ID of the plugin to execute
+     */
     public void execute(String pluginID) {
+    	//Get the plugin
         ResourceBundle properties = getResourceBundle(pluginID);
      
         if (properties == null) {
@@ -61,6 +80,7 @@ public class StaticExecutableRunner {
         
         String baseexecutable = properties.getString(pluginID + ".baseexecutable");
         
+        //Get the system's environment
         String os = Platform.getOS();
         String arch = Platform.getOSArch();
         
@@ -70,6 +90,7 @@ public class StaticExecutableRunner {
             baseexecutable += ".exe";
         }
         
+        //Create the directory to execute
         String executableDir = properties.getString(pluginID + "." + platform);
         executableDir = pluginPath + subdirectory + File.separator + executableDir;
         
@@ -95,25 +116,33 @@ public class StaticExecutableRunner {
             }
         }
         
+        //execute the program getting all of the new files
         File[] outputs = execute(tempDirectory, baseexecutable);
         
         if (outputs.length > 0) {
+        	//New files were created, get persisters for them
+        	ArrayList arrayList = new ArrayList();
             //OutputtedFilesDialog.showGUI(outputs);
+        	
+            String pluginName = properties.getString(pluginID + ".name");
+            
         	for (int i = 0; i < outputs.length; i++) {
-        		Object        model = null;
-        		String        label = null;
-        		DataModelType type  = null; 
-        		
+            	DataModel dataModel = null;
         		if (fileToInfoMap.containsKey(outputs[i].getName())) {
+        			//there was a specialized persister registered for this file
         			FileInfo fileInfo = (FileInfo)fileToInfoMap.get(outputs[i].getName());
         			
-        			fileInfo.setFileDesc(new BasicFileResourceDescriptor(outputs[i]));
-
         			Persister p = fileInfo.getPersister();
         		    try {
-						model = p.restore(fileInfo.getFileDesc());
-						label = fileInfo.getTitle();
-						type  = fileInfo.getType();
+						dataModel = new BasicDataModel(p.restore(new BasicFileResourceDescriptor(outputs[i])));
+						dataModel.getProperties().setPropertyValue(DataModelProperty.LABEL, 
+								                                   fileInfo.getTitle() + 
+								                                   " (" +
+								                                   pluginName +
+								                                   ")");
+						dataModel.getProperties().setPropertyValue(DataModelProperty.TYPE,
+								                                   fileInfo.getType());
+						
 					} catch (OutOfMemoryError e) {
 						e.printStackTrace();
 					} catch (IOException e) {
@@ -122,14 +151,15 @@ public class StaticExecutableRunner {
 						e.printStackTrace();
 					}	
         		} else {
+        			//Use the general file persister for the file
         			PersistenceRegistry perReg = IVC.getInstance()
 							.getPersistenceRegistry();
 					try {
-						model = perReg.load(new BasicFileResourceDescriptor(
-								outputs[i]));
-						//label = outputs[i].toString();
-						label = outputs[i].getName();
-						type  = DataModelType.OTHER;
+						dataModel = 
+							new BasicDataModel(perReg.load(new BasicFileResourceDescriptor(outputs[i])));
+						dataModel.getProperties().setPropertyValue(DataModelProperty.LABEL,
+								outputs[i].getName() + " (" + pluginName + ")");
+						dataModel.getProperties().setPropertyValue(DataModelProperty.TYPE, DataModelType.OTHER);
 					} catch (OutOfMemoryError e) {
 						e.printStackTrace();
 					} catch (IOException e) {
@@ -139,44 +169,56 @@ public class StaticExecutableRunner {
 					}
 				}
         		
-        		if (model != null && label != null && type != null) {
-					DataModel dataModel = new BasicDataModel(model);
-					PropertyMap propMap = dataModel.getProperties();
-					propMap.put(DataModelProperty.LABEL, label);
-					propMap.put(DataModelProperty.TYPE, type);
-					try {
+        		if (outputs[i].getName().equals(this.parentFilename) && dataModel != null) {
+        			//There exists a parent data model for all output of this plugin
+        			this.parentDataModel = dataModel;
+        			try {
 						IVC.getInstance().addModel(dataModel);
 					} catch (UnsupportedModelException e) {
 						e.printStackTrace();
 					}
+        		}
+        		else {
+        			//else just add the model to the list
+        			arrayList.add(dataModel);
+        		}
+        		
+        	} //end for
+
+        	//Add the datamodels to IVC
+        	for (int i = 0; i < arrayList.size(); ++i) {
+        		DataModel dm = (DataModel)arrayList.get(i);
+        		if (this.parentDataModel != null) {
+        			dm.getProperties().setPropertyValue(DataModelProperty.PARENT,
+        					                            this.parentDataModel);
+        		}
+        		try {
+					IVC.getInstance().addModel(dm);
+				} catch (UnsupportedModelException e) {
+					e.printStackTrace();
 				}
         	}
+        	
         } else {
             IVC.getInstance().getConsole().print("No output detected.");
         }
-        
-        //remove(getTempDirectory());
     }
     
-/*    
-    private void remove(File file) {
-        if (file.isDirectory()) {
-            File[] files = file.listFiles();
-            
-            for (int i=0; i < files.length; i++) {
-                remove(files[i]);
-            }
-        }
-        
-        file.delete();
-    }
- */
-    
+    /**
+     * Get the temporary directory this class is running in
+     * @return The temporary file
+     */
     public File getTempDirectory() {
         return new File(tempDirectory);
     }
     
-    public File[] execute(String basedirectory, String executable) {        
+    /**
+     * Drops to the command line and executes the program
+     * @param basedirectory The directory where the necessary files to execute reside
+     * @param executable The name of the program to execute
+     * @return List of generated files
+     */
+    private File[] execute(String basedirectory, String executable) {        
         String execFile = basedirectory + File.separator + executable;
         
         File dir = new File(basedirectory);
@@ -185,6 +227,7 @@ public class StaticExecutableRunner {
         if (arguments == null) arguments = "";
         
         try {
+        	//Drops to console to execute
             Runtime.getRuntime().exec(execFile + " " + arguments, null, new File(basedirectory)).waitFor();
             
             IVC.getInstance().getConsole().print("Execution of " + executable + " complete.\n");
@@ -194,7 +237,7 @@ public class StaticExecutableRunner {
             IVC.showError("Error Executing!",e.getMessage(),null);;
         }
         
-        
+        //get the outputted files
         String[] afterFiles = dir.list();
         
         Arrays.sort(beforeFiles);
@@ -226,9 +269,10 @@ public class StaticExecutableRunner {
     }
     
     /**
-     * 
-     * @param from
-     * @param to
+     * Copies files whether they are directories or regular files to the destination
+     * directory
+     * @param from File to copy from
+     * @param to File to copy to
      */
     private void copy(File from, File to) {
         if (from.isDirectory()) {            
@@ -266,6 +310,10 @@ public class StaticExecutableRunner {
         }
     }
     
+    /**
+     * Create a temporary directory for the executable runner
+     * @return The name of the temporary directory
+     */
     private String makeTempDirectory() {
         String temp = IVC.getInstance().getTemporaryFilesFolder();
         File dir = null;
@@ -281,10 +329,19 @@ public class StaticExecutableRunner {
         return dir.toString();
     }
     
+    /**
+     * Prints an error message to IVC
+     * @param msg The error message
+     */
     protected void errorLog(String msg) {
         IVC.getInstance().getErrorLogger().error(msg);
     }
     
+    /**
+     * Retrieve the properties of the plugin (file location, architecture, etc)
+     * @param pluginID The name of the plugin
+     * @return Properties of the plugin
+     */
     private ResourceBundle getResourceBundle(String pluginID) {
         String pluginFSPath = IVC.getInstance().getPluginPath(pluginID);
         
@@ -300,22 +357,64 @@ public class StaticExecutableRunner {
         return null;
     }
     
+    /**
+     * Sets any arguments to pass through the command line
+     * @param args The argument to pass
+     */
     public void setArguments(String args) {
         this.arguments = args;
     }
     
+    /**
+     * Get the current arguments
+     * @return The arguments
+     */
     public String getArguments() {
         return arguments;
     }
     
+    /**
+     * Set the extra files needed
+     * @param files An array of files
+     */
     public void setExtraFilesNeeded(File[] files) {
         extraFiles = files;
     }
     
+    /**
+     * Retrieve the extra files needed by the plugin
+     * @return Array of files
+     */
     public File[] getExtraFilesNeeded() {
         return extraFiles;
     }
     
+    /**
+     * Set the datamodel's filename that will be the parent of all the
+     * output produced.  The filename will be a product of the algorithm
+     * executed.
+     * @param filename Filename of parent
+     */
+    public void setParentFilename(String filename) {
+    	this.parentFilename = filename;
+    }
+    
+    /**
+     * Parent datamodel for all of the output produced.
+     * @param dm Parent datamodel
+     */
+    public void setParentDataModel(DataModel dm) {
+    	this.parentDataModel = dm;
+    }
+    
+    /**
+     * Register a file with a persister.
+     * 
+     * @param filename Filename of the persister
+     * @param title Title of datamodel
+     * @param type Type of data (network, text file, etc)
+     * @param persister The persister of the file
+     */
     public void registerFile(String filename, String title, 
     		                DataModelType type, Persister persister) {
     	
@@ -326,46 +425,59 @@ public class StaticExecutableRunner {
     }
     
     /**
+     * Holds the fileinfo for any registered files
      * 
      * @author Team IVC
-     * 
      */
     private class FileInfo {
-    	private FileResourceDescriptor fileDesc;
     	private String                 filename;
     	private String                 title;
     	private DataModelType          type;
     	private Persister              persister;
     	
+    	/**
+    	 * Populate the file data
+    	 * @param filename Name of the file
+    	 * @param title Title of the datamodel
+    	 * @param type Type of datamodel contained in the file
+    	 * @param persister Persister handling the file
+    	 */
     	public void setInfo(String filename, String title,
     			            DataModelType type, Persister persister) {
     		this.filename  = filename;
     		this.title     = title;
     		this.type      = type;
     		this.persister = persister;
-    		this.fileDesc  = null;
     	}
     	
-    	public void setFileDesc(FileResourceDescriptor frd) {
-    		this.fileDesc = frd;
-    	}
-    	
+    	/**
+    	 * The filename
+    	 * @return filename
+    	 */
     	public String getFilename() {
     		return this.filename;
     	}
-    	
-    	public FileResourceDescriptor getFileDesc() {
-    		return this.fileDesc;
-    	}
-    	
+    	    	
+    	/**
+    	 * Title of the datamodel produced
+    	 * @return Title
+    	 */
     	public String getTitle() {
     		return this.title;
     	}
     	
+    	/**
+    	 * Type of datamodel produced
+    	 * @return Datamodel type
+    	 */
     	public DataModelType getType() {
     		return this.type;
     	}
     	
+    	/**
+    	 * The persister of the file
+    	 * @return Persister
+    	 */
     	public Persister getPersister() {
     		return this.persister;
     	}
