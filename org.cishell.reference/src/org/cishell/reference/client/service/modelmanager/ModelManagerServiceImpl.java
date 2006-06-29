26a72aca95eac2bc48ac83b4dfabb4923a943617
bh2 <bh2@138b4e90-db10-0410-b6da-c669c309eabc>
2006-06-29 14:19:44 +0000

    initial commit of the new cishell

---
+++ org.cishell.reference/src/org/cishell/reference/client/service/modelmanager/ModelManagerServiceImpl.java
@@ -0,0 +1,194 @@
+/* **************************************************************************** 
+ * CIShell: Cyberinfrastructure Shell, An Algorithm Integration Framework.
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Apache License v2.0 which accompanies
+ * this distribution, and is available at:
+ * http://www.apache.org/licenses/LICENSE-2.0.html
+ * 
+ * Created on Jun 15, 2006 at Indiana University.
+ * 
+ * Contributors:
+ *     Indiana University - 
+ * ***************************************************************************/
+package org.cishell.reference.client.service.modelmanager;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.cishell.client.service.modelmanager.ModelManagerListener;
+import org.cishell.client.service.modelmanager.ModelManagerService;
+import org.cishell.framework.datamodel.DataModel;
+import org.cishell.framework.datamodel.DataModelProperty;
+import org.cishell.framework.datamodel.DataModelType;
+
+
+public class ModelManagerServiceImpl implements ModelManagerService {
+    private Map modelToLabelMap;
+    private Map labelToModelMap; 
+    private Map substringToNumberMap;
+    private Set models;
+    private Set selectedModels;
+    
+    private Set listeners;
+    
+    /**
+     * Creates a new BasicModelManager Object.
+     */
+    public ModelManagerServiceImpl() {
+        modelToLabelMap = new HashMap();
+        labelToModelMap = new HashMap();
+        substringToNumberMap = new HashMap();
+        models = new HashSet();
+        listeners = new HashSet();
+    }
+
+    /**
+     * @see edu.iu.iv.core.ModelManager#addModel(java.lang.Object)
+     */
+    public void addModel(DataModel model) {
+        String label = (String)model.getProperties().get(DataModelProperty.LABEL);
+        DataModelType type = (DataModelType)model.getProperties().get(DataModelProperty.TYPE);
+        
+        if(type == null){
+            type = DataModelType.OTHER;
+            model.getProperties().put(DataModelProperty.TYPE, type);
+        }
+        
+        //generate label if needed
+        if(label == null || label.equals("")){
+            StackTraceElement[] stack = new Throwable().getStackTrace();                
+            
+            if (stack.length > 2) {
+                String className = stack[2].getClassName();
+                int lastDot = className.lastIndexOf(".");
+                
+                if (className.length() > lastDot) {
+                    lastDot++;
+                    className = className.substring(lastDot);
+                    
+                    if (className.endsWith("Algorithm")) {
+                        className = className.substring(0,className.lastIndexOf("Algorithm"));
+                    }
+                    
+                    if (className.endsWith("Plugin")) {
+                        className = className.substring(0,className.lastIndexOf("Plugin"));
+                    }
+                }           
+                label = className;  
+            } else {
+                label = "Unknown";
+            }
+            
+            label = label + "." + type.getName();           
+        }
+        
+        addModel(model, label);
+        
+        for (Iterator iter=listeners.iterator(); iter.hasNext();) {
+            ((ModelManagerListener) iter.next()).modelAdded(model);
+        }
+    }
+
+    private void addModel(DataModel model, String label) {
+        label = findUniqueLabel(label);
+        model.getProperties().put(DataModelProperty.LABEL, label);
+        //set the model to be unsaved initially
+        model.getProperties().put(DataModelProperty.MODIFIED, new Boolean(true));
+                
+        modelToLabelMap.put(model, label);
+        labelToModelMap.put(label, model);      
+        models.add(model);
+    }
+    
+    private String findUniqueLabel(String label) {
+        int lastIndex = label.length() - 1;
+        boolean foundNumber = false;
+        
+        while (lastIndex > 0 && Character.isDigit(label.charAt(lastIndex))) {
+            foundNumber = true;
+            lastIndex--;
+        }
+        
+        String newLabel = label.substring(0,lastIndex + 1);
+        if (newLabel.charAt(newLabel.length()-1) != ' ') {
+            newLabel = newLabel + ".";
+        }
+        
+        
+        Integer oldNumber = (Integer) substringToNumberMap.get(newLabel);
+        if (oldNumber != null) {
+            oldNumber = new Integer(oldNumber.intValue() + 1);
+        } else {
+            oldNumber = new Integer(1);
+        }
+        
+        substringToNumberMap.put(newLabel,oldNumber);
+        
+        if (foundNumber) {
+            int oldNum = oldNumber.intValue();
+            int number = Integer.parseInt(label.substring(lastIndex+1));
+            
+            if (number < oldNum && getModelForLabel(newLabel + number) == null) {
+                return newLabel + number;
+            }
+        } 
+        
+        newLabel = newLabel + oldNumber;
+        
+        return newLabel;
+    }
+
+
+    public void removeModel(DataModel model) {
+        String label = getLabelForModel(model);
+        
+        labelToModelMap.remove(label);
+        modelToLabelMap.remove(model);
+        models.remove(model);
+        
+        for (Iterator iter=listeners.iterator(); iter.hasNext();) {
+            ((ModelManagerListener) iter.next()).modelRemoved(model);
+        }
+    }
+
+    public DataModel[] getSelectedModels() {
+        if (selectedModels == null) {
+            selectedModels = new HashSet();
+        }
+        
+        return (DataModel[]) selectedModels.toArray(new DataModel[]{});
+    }
+
+    public void setSelectedModels(DataModel[] models) {
+        selectedModels = new HashSet(Arrays.asList(models));
+        
+        for (Iterator iter=listeners.iterator(); iter.hasNext();) {
+            ((ModelManagerListener) iter.next()).modelsSelected(models);
+        }
+    }
+    
+    private DataModel getModelForLabel(String label){
+        return (DataModel)labelToModelMap.get(label);
+    }
+    
+    private String getLabelForModel(DataModel model){
+        return (String)modelToLabelMap.get(model);
+    }
+
+    public DataModel[] getModels() {
+        return (DataModel[]) models.toArray(new DataModel[]{});
+    }
+
+    public void addModelManagerListener(ModelManagerListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeModelManagerListener(ModelManagerListener listener) {
+        listeners.remove(listener);
+    }
+}
---
+++ org.cishell.reference/src/org/cishell/reference/service/conversion/DataConversionServiceImpl.java
@@ -0,0 +1,64 @@
+/* **************************************************************************** 
+ * CIShell: Cyberinfrastructure Shell, An Algorithm Integration Framework.
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Apache License v2.0 which accompanies
+ * this distribution, and is available at:
+ * http://www.apache.org/licenses/LICENSE-2.0.html
+ * 
+ * Created on Jun 15, 2006 at Indiana University.
+ * 
+ * Contributors:
+ *     Indiana University - 
+ * ***************************************************************************/
+package org.cishell.reference.service.conversion;
+
+import org.cishell.framework.CIShellContext;
+import org.cishell.framework.algorithm.AlgorithmFactory;
+import org.cishell.framework.algorithm.AlgorithmProperty;
+import org.cishell.service.conversion.DataConversionService;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.log.LogService;
+
+public class DataConversionServiceImpl implements DataConversionService, AlgorithmProperty {
+    private BundleContext bcontext;
+    private CIShellContext ciContext;
+    
+    public DataConversionServiceImpl(BundleContext bcontext, CIShellContext ciContext) {
+        this.bcontext = bcontext;
+        this.ciContext = ciContext;
+    }
+
+    /**
+     * @see org.cishell.service.conversion.DataConversionService#converterFor(java.lang.String, java.lang.String)
+     */
+    public AlgorithmFactory converterFor(String inFormat, String outFormat) {
+        try {
+            String filter = "(&("+IN_DATA+"="+inFormat+") " +
+                              "("+OUT_DATA+"="+outFormat+"))";
+            
+            ServiceReference[] refs = bcontext.getServiceReferences(
+                    AlgorithmFactory.class.getName(), filter);
+            if (refs != null && refs.length > 0) {
+                return (AlgorithmFactory)bcontext.getService(refs[0]);
+            }
+        } catch (InvalidSyntaxException e) {
+            getLog().log(LogService.LOG_ERROR, "Incorrect Syntax", e);
+        }
+        return null;
+    }
+
+    /**
+     * @see org.cishell.service.conversion.DataConversionService#converterFor(java.lang.String, java.lang.String, int, java.lang.String)
+     */
+    public AlgorithmFactory converterFor(String inFormat, String outFormat,
+            int maxHops, String maxComplexity) {
+        return converterFor(inFormat, outFormat);
+    }
+    
+    private LogService getLog() {
+        return (LogService)ciContext.getService(LogService.class.getName());
+    }
+}
