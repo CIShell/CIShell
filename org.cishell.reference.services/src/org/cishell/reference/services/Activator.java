17eab44d24abd1b0a8557ee633056734bc552266
bh2 <bh2@138b4e90-db10-0410-b6da-c669c309eabc>
2006-07-24 21:23:35 +0000

    Committing the cishell reference services starter. This bundle will start some reference services at startup. Applications with custom implimentations will probably not use this and register the services themselves.

---
+++ org.cishell.reference.services/src/org/cishell/reference/services/Activator.java
@@ -0,0 +1,42 @@
+package org.cishell.reference.services;
+
+import java.util.Hashtable;
+
+import org.cishell.app.service.scheduler.SchedulerService;
+import org.cishell.framework.CIShellContext;
+import org.cishell.framework.LocalCIShellContext;
+import org.cishell.reference.client.service.scheduler.SchedulerServiceImpl;
+import org.cishell.reference.service.conversion.DataConversionServiceImpl;
+import org.cishell.service.conversion.DataConversionService;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public class Activator implements BundleActivator {
+    private ServiceRegistration conversionReg;
+    private ServiceRegistration schedulerReg;
+
+	/**
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bContext) throws Exception {
+        CIShellContext ciContext = new LocalCIShellContext(bContext);
+        
+        DataConversionService conversionService = 
+            new DataConversionServiceImpl(bContext, ciContext);
+        conversionReg = bContext.registerService(
+                DataConversionService.class.getName(), conversionService, new Hashtable());
+        
+        SchedulerService scheduler = new SchedulerServiceImpl();
+        schedulerReg = bContext.registerService(
+                SchedulerService.class.getName(), scheduler, new Hashtable());
+	}
+
+	/**
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bContext) throws Exception {
+        conversionReg.unregister();
+        schedulerReg.unregister();
+	}
+}
