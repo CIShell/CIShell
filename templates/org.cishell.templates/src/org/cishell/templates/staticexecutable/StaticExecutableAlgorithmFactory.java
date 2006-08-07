41704fa04a371df2b1b3e8debb25146985ed0ccd
bh2 <bh2@138b4e90-db10-0410-b6da-c669c309eabc>
2006-08-07 19:47:21 +0000

    Initial commit of the cishell templates. currently only has the static executable template

---
+++ templates/org.cishell.templates/src/org/cishell/templates/staticexecutable/StaticExecutableAlgorithmFactory.java
@@ -0,0 +1,178 @@
+/* **************************************************************************** 
+ * CIShell: Cyberinfrastructure Shell, An Algorithm Integration Framework.
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Apache License v2.0 which accompanies
+ * this distribution, and is available at:
+ * http://www.apache.org/licenses/LICENSE-2.0.html
+ * 
+ * Created on Jul 31, 2006 at Indiana University.
+ * 
+ * Contributors:
+ *     Indiana University - 
+ * ***************************************************************************/
+package org.cishell.templates.staticexecutable;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Properties;
+
+import org.cishell.framework.CIShellContext;
+import org.cishell.framework.algorithm.Algorithm;
+import org.cishell.framework.algorithm.AlgorithmFactory;
+import org.cishell.framework.data.Data;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.metatype.MetaTypeProvider;
+import org.osgi.service.metatype.MetaTypeService;
+
+
+public class StaticExecutableAlgorithmFactory implements AlgorithmFactory {
+    BundleContext bContext;
+    String algName;
+    MetaTypeProvider provider;
+
+    protected void activate(ComponentContext ctxt) {
+        bContext = ctxt.getBundleContext();
+        algName = (String)ctxt.getProperties().get("Algorithm-Directory");
+        
+        try {
+            MetaTypeService mts = (MetaTypeService) ctxt.locateService("MTS");
+            provider = mts.getMetaTypeInformation(ctxt.getBundleContext().getBundle());
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+    protected void deactivate(ComponentContext ctxt) {
+        bContext = null;
+        algName = null;
+    }
+
+    public Algorithm createAlgorithm(Data[] data, Dictionary parameters, CIShellContext context) {
+        return new StaticExecutableAlgorithm(data, parameters, context);
+    }
+
+    public MetaTypeProvider createParameters(Data[] data) {
+        return provider;
+    }
+    
+    private class StaticExecutableAlgorithm implements Algorithm {
+        Data[] data;
+        Dictionary parameters;
+        CIShellContext context;
+        
+        public StaticExecutableAlgorithm(Data[] data, Dictionary parameters, CIShellContext context) {
+            this.data = data;
+            this.parameters = parameters;
+            this.context = context;
+        }
+
+        public Data[] execute() {
+            try {
+                Properties serviceProps = getProperties("/"+algName+"/service.properties");
+                Properties configProps = getProperties("/"+algName+"/config.properties");
+                
+                serviceProps.putAll(configProps);
+                serviceProps.put("Algorithm-Directory", algName);
+                
+                StaticExecutableRunner runner = 
+                    new StaticExecutableRunner(bContext, context, serviceProps, parameters, data);
+                
+                copyFiles(runner.getTempDirectory());
+            
+                return runner.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
+        
+        private void copyFiles(File dir) throws IOException {
+            Enumeration e = bContext.getBundle().getEntryPaths("/"+algName);
+            
+            dir = new File(dir.getPath() + File.separator + algName);
+            dir.mkdirs();
+            
+            String os = bContext.getProperty("osgi.os");
+            String arch = bContext.getProperty("osgi.arch");
+            
+            while (e != null && e.hasMoreElements()) {
+                String path = (String)e.nextElement();
+                
+                if (path.endsWith("/")) {
+                    if (path.endsWith("default/") || 
+                            path.endsWith(os+"."+arch+"/") ||
+                            path.endsWith("win32/") && os.equals("win32")) {
+                        copyDir(dir, path);
+                    }
+                } else {
+                    //copyFile(dir, path);
+                }
+            }
+        }
+        
+        private void copyDir(File dir, String dirPath) throws IOException {
+            Enumeration e = bContext.getBundle().getEntryPaths(dirPath);
+            
+            dirPath = dirPath.replace('/', File.separatorChar);
+            
+            while (e != null && e.hasMoreElements()) {
+                String path = (String)e.nextElement();
+                
+                if (path.endsWith("/")) {
+                    String dirName = getName(path);
+                    
+                    dir = new File(dir.getPath() + File.separator + dirName);
+                    dir.mkdirs();
+                    copyDir(dir, path);
+                } else {
+                    copyFile(dir, path);
+                }
+            }
+        }
+        
+        private void copyFile(File dir, String path) throws IOException {
+            URL entry = bContext.getBundle().getEntry(path);
+            
+            path = path.replace('/', File.separatorChar);
+            String file = getName(path);
+            FileOutputStream outStream = new FileOutputStream(dir.getPath() + File.separator + file);
+            
+            ReadableByteChannel in = Channels.newChannel(entry.openStream());
+            FileChannel out = outStream.getChannel();
+            out.transferFrom(in, 0, Integer.MAX_VALUE);
+            
+            in.close();
+            out.close();
+        }
+        
+        private String getName(String path) {
+            if (path.lastIndexOf(File.separator) == path.length()-1) {
+                path = path.substring(0, path.length()-1);
+            }
+            
+            path = path.substring(path.lastIndexOf(File.separatorChar)+1, 
+                    path.length());
+            
+            return path;
+        }
+        
+        private Properties getProperties(String entry) throws IOException {
+            URL url = bContext.getBundle().getEntry(entry);
+            Properties props = null;
+            
+            if (url != null) {
+                props = new Properties();
+                props.load(url.openStream());
+            }
+            return props;
+        }
+    }
+}
---
+++ templates/org.cishell.templates/src/org/cishell/templates/staticexecutable/StaticExecutableRunner.java
@@ -0,0 +1,234 @@
+/* **************************************************************************** 
+ * CIShell: Cyberinfrastructure Shell, An Algorithm Integration Framework.
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Apache License v2.0 which accompanies
+ * this distribution, and is available at:
+ * http://www.apache.org/licenses/LICENSE-2.0.html
+ * 
+ * Created on Aug 4, 2006 at Indiana University.
+ * 
+ * Contributors:
+ *     Indiana University - 
+ * ***************************************************************************/
+package org.cishell.templates.staticexecutable;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.cishell.framework.CIShellContext;
+import org.cishell.framework.algorithm.Algorithm;
+import org.cishell.framework.algorithm.AlgorithmProperty;
+import org.cishell.framework.data.BasicData;
+import org.cishell.framework.data.Data;
+import org.cishell.framework.data.DataProperty;
+import org.cishell.templates.Activator;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.log.LogService;
+
+/**
+ * 
+ * @author Bruce Herr (bh2@bh2.net)
+ */
+public class StaticExecutableRunner implements Algorithm {
+    protected final String tempDir;
+    protected final Data[] data;
+    protected Dictionary parameters;
+    protected Properties props;
+    protected CIShellContext ciContext;
+
+    public StaticExecutableRunner(BundleContext bContext, CIShellContext ciContext, Properties props, Dictionary parameters, Data[] data) throws IOException {
+        this.ciContext = ciContext;
+        this.props = props;
+        this.parameters = parameters;
+        this.data = data;
+        
+        if (data == null) data = new Data[0];
+        if (parameters == null) parameters = new Hashtable();
+        
+        tempDir = makeTempDirectory();
+    }
+    
+    /**
+     * @see org.cishell.framework.algorithm.Algorithm#execute()
+     */
+    public Data[] execute() {
+        try {
+            String algDir = tempDir + File.separator + props.getProperty("Algorithm-Directory") + File.separator;
+            
+            chmod(algDir);
+            File[] output = execute(algDir + getTemplate(), algDir);
+            
+            return toData(output);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+    
+    protected Data[] toData(File[] files) {
+        String[] formats = ((String)props.get(AlgorithmProperty.OUT_DATA)).split(",");
+        
+        Map nameToFileMap = new HashMap();
+        for (int i=0; i < files.length; i++) {
+            nameToFileMap.put(files[i].getName(), files[i]);
+        }
+        
+        Data[] data = null;
+        if (formats.length > files.length) {
+            data = new Data[formats.length];
+        } else {
+            data = new Data[files.length];
+        }
+        
+        for (int i=0; i < data.length; i++) {
+            String file = props.getProperty("outFile["+i+"]", null);
+            
+            if (i < formats.length) {
+                File f = (File) nameToFileMap.remove(file);
+                
+                if (f != null) {
+                    data[i] = new BasicData(f,formats[i]);
+                    
+                    String label = props.getProperty("outFile["+i+"].label", f.getName());
+                    data[i].getMetaData().put(DataProperty.LABEL, label);
+                }
+            } else {
+                Iterator iter = nameToFileMap.values().iterator();
+                while (iter.hasNext()) {
+                    File f = (File) iter.next();
+                    
+                    data[i] = new BasicData(f, "file:text/plain");
+                    data[i].getMetaData().put(DataProperty.LABEL, f.getName());
+                    
+                    i++;
+                }
+                break;
+            }
+        }
+        
+        return data;
+    }
+    
+    protected void chmod(String baseDir) {
+        //FIXME: Surely java has a way to do this!!!!
+        if (new File("/bin/chmod").exists()) {
+            try {
+                String executable = baseDir + props.getProperty("executable");
+                Runtime.getRuntime().exec("/bin/chmod +x " + executable).waitFor();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    protected File[] execute(String execString, String baseDir) throws Exception {
+        File dir = new File(baseDir);
+        String[] beforeFiles = dir.list();
+        
+        Process process = Runtime.getRuntime().exec(execString, null, new File(baseDir));
+
+        logStream(LogService.LOG_INFO, process.getInputStream());
+        logStream(LogService.LOG_ERROR, process.getErrorStream());
+        process.waitFor();
+        
+        //get the outputted files
+        String[] afterFiles = dir.list();
+        
+        Arrays.sort(beforeFiles);
+        Arrays.sort(afterFiles);
+        
+        List outputs = new ArrayList();
+        String tempDir = this.tempDir + File.separator;
+        
+        int beforeIndex = 0;
+        int afterIndex = 0;
+        
+        while (beforeIndex < beforeFiles.length && afterIndex < afterFiles.length) {
+            if (beforeFiles[beforeIndex].equals(afterFiles[afterIndex])) {
+                beforeIndex++;
+                afterIndex++;
+            } else {
+                outputs.add(new File(tempDir + afterFiles[afterIndex]));
+                afterIndex++;
+            }
+        }  
+        
+        //get any remaining new files
+        while (afterIndex < afterFiles.length) {
+            outputs.add(new File(tempDir + afterFiles[afterIndex]));
+            afterIndex++;
+        }
+        
+        return (File[]) outputs.toArray(new File[]{});
+    }
+    
+    protected void logStream(int logLevel, InputStream is) {
+        LogService log = (LogService) ciContext.getService(LogService.class.getName());
+        
+        if (log == null) return;
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+        try {
+            String line = reader.readLine();
+            while (line != null) {
+                log.log(logLevel, line);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected String getTemplate() {
+        String template = props.getProperty("template");
+        
+        template = template.replaceAll("\\$\\{executable\\}", props.getProperty("executable"));
+        
+        for (int i=0; i < data.length; i++) {
+            String file = ((File) data[i].getData()).getAbsolutePath();
+            template = template.replaceAll("\\$\\{inFile\\["+i+"\\]\\}", "\""+file+"\"");
+        }
+        
+        for (Enumeration i=parameters.keys(); i.hasMoreElements(); ) {
+            String key = (String)i.nextElement();
+            Object value = parameters.get(key);
+            
+            if (value == null) value = "";
+            
+            template = template.replaceAll("\\$\\{"+key+"\\}", value.toString());
+        }
+        
+        return template;
+    }
+    
+    public File getTempDirectory() {
+        return new File(tempDir);
+    }
+    
+    protected String makeTempDirectory() throws IOException {
+        File sessionDir = Activator.getTempDirectory();
+        File dir = File.createTempFile("StaticExecutableRunner-", "", sessionDir);
+        
+        dir.delete();
+        dir.mkdirs();
+        
+        return dir.getAbsolutePath();
+    }
+}
