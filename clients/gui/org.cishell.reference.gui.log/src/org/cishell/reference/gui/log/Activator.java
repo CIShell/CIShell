ca37446929ee72f581be4da02385b8e2b192e2f3
bh2 <bh2@138b4e90-db10-0410-b6da-c669c309eabc>
2006-08-23 23:18:37 +0000

    made Log view print to system.out for now until someone creates the real gui for it

--- clients/gui/org.cishell.reference.gui.log/src/org/cishell/reference/gui/log/Activator.java
+++ clients/gui/org.cishell.reference.gui.log/src/org/cishell/reference/gui/log/Activator.java
@@ -2,49 +2,57 @@ package org.cishell.reference.gui.log;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.log.LogEntry;
+import org.osgi.service.log.LogListener;
+import org.osgi.service.log.LogReaderService;
 
 /**
  * The activator class controls the plug-in life cycle
  */
 public class Activator extends AbstractUIPlugin {
-
-	// The plug-in ID
 	public static final String PLUGIN_ID = "org.cishell.reference.gui.log";
-
-	// The shared instance
 	private static Activator plugin;
 	
-	/**
-	 * The constructor
-	 */
 	public Activator() {
 		plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
+        
+        LogListener listener = new LogListener() {
+            public void logged(LogEntry e) {
+                if (goodMessage(e.getMessage())) {
+                    System.out.println(e.getMessage());
+                }
+            }
+            
+            public boolean goodMessage(String msg) {
+                if (msg == null || 
+                        msg.startsWith("ServiceEvent ") || 
+                        msg.startsWith("BundleEvent ") || 
+                        msg.startsWith("FrameworkEvent ")) {
+                    return false;
+                } else {
+                    return true;   
+                }
+            }
+        };
+        
+        ServiceReference ref = context.getServiceReference(LogReaderService.class.getName());
+        LogReaderService reader = (LogReaderService) context.getService(ref);
+        if (reader != null) {
+            reader.addLogListener(listener);
+        }
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
 	public static Activator getDefault() {
 		return plugin;
 	}
-
 }
