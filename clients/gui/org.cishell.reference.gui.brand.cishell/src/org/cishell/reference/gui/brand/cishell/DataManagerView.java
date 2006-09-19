2e2de20569026fb20a3006ea72f6f9fe0add886e
bearsfan <bearsfan@138b4e90-db10-0410-b6da-c669c309eabc>
2006-09-19 17:51:54 +0000

    Set up a brand for the data manager view

---
+++ clients/gui/org.cishell.reference.gui.brand.cishell/src/org/cishell/reference/gui/brand/cishell/DataManagerView.java
@@ -0,0 +1,9 @@
+package org.cishell.reference.gui.brand.cishell;
+
+import org.cishell.reference.gui.datamanager.AbstractDataManagerView;
+
+public class DataManagerView extends AbstractDataManagerView {
+	public DataManagerView() {
+		super("org.cishell.reference.gui.brand.cishell");
+	}
+}
