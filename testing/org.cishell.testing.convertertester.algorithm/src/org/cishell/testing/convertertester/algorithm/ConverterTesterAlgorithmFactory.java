ad8c1863aec554868a22224962b03d3224ab3d42
mwlinnem <mwlinnem@138b4e90-db10-0410-b6da-c669c309eabc>
2007-08-17 19:47:55 +0000

    Removed unnecessary import.

--- testing/org.cishell.testing.convertertester.algorithm/src/org/cishell/testing/convertertester/algorithm/ConverterTesterAlgorithmFactory.java
+++ testing/org.cishell.testing.convertertester.algorithm/src/org/cishell/testing/convertertester/algorithm/ConverterTesterAlgorithmFactory.java
@@ -9,7 +9,6 @@ import org.cishell.framework.data.Data;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.metatype.MetaTypeProvider;
-import org.osgi.service.metatype.MetaTypeService;
 
 
 public class ConverterTesterAlgorithmFactory implements AlgorithmFactory {
