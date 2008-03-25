538d4bab70345be359b12194c158be75eba5aaf3
bh2 <bh2@138b4e90-db10-0410-b6da-c669c309eabc>
2008-03-25 14:16:38 +0000

    moved 1.0 framework bundle to trunk

---
+++ core/org.cishell.framework/src/org/cishell/service/conversion/ConversionException.java
@@ -0,0 +1,33 @@
+/* **************************************************************************** 
+ * CIShell: Cyberinfrastructure Shell, An Algorithm Integration Framework.
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Apache License v2.0 which accompanies
+ * this distribution, and is available at:
+ * http://www.apache.org/licenses/LICENSE-2.0.html
+ * 
+ * Created on Feb 8, 2008 at Indiana University.
+ * 
+ * Contributors:
+ *     Indiana University - Initial API
+ * ***************************************************************************/
+package org.cishell.service.conversion;
+
+
+/**
+ * An exception which is thrown when an error occurs in the process of data 
+ * conversion
+ * 
+ * @author Bruce Herr (bh2@bh2.net)
+ */
+public class ConversionException extends Exception {
+	private static final long serialVersionUID = 1749134893481511313L;
+
+	public ConversionException(Exception exception) {
+		super(exception);
+	}
+	
+	public ConversionException(String message) {
+		super(message);
+	}
+}
