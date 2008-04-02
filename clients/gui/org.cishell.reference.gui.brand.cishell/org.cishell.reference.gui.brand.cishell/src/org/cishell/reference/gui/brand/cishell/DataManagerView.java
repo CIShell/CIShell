0d4384fe0c92c95b5aabef1cb22c2e498ae3dba1
mwlinnem <mwlinnem@138b4e90-db10-0410-b6da-c669c309eabc>
2008-04-02 17:57:22 +0000

    This never should have been here in the first place.

--- clients/gui/org.cishell.reference.gui.brand.cishell/org.cishell.reference.gui.brand.cishell/src/org/cishell/reference/gui/brand/cishell/DataManagerView.java
+++
@@ -1,9 +0,0 @@
-package org.cishell.reference.gui.brand.cishell;
-
-import org.cishell.reference.gui.datamanager.AbstractDataManagerView;
-
-public class DataManagerView extends AbstractDataManagerView {
-	public DataManagerView() {
-		super("org.cishell.reference.gui.brand.cishell");
-	}
-}
