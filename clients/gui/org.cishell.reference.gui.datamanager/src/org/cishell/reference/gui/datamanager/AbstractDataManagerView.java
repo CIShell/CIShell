5c03efed070188ae3a6528ecef0b2268607294eb
mwlinnem <mwlinnem@138b4e90-db10-0410-b6da-c669c309eabc>
2008-07-08 17:47:55 +0000

    test.

--- clients/gui/org.cishell.reference.gui.datamanager/src/org/cishell/reference/gui/datamanager/AbstractDataManagerView.java
+++ clients/gui/org.cishell.reference.gui.datamanager/src/org/cishell/reference/gui/datamanager/AbstractDataManagerView.java
@@ -109,6 +109,7 @@ public abstract class AbstractDataManagerView extends ViewPart implements
 	private LogService log;
 
 	public AbstractDataManagerView(String brandPluginID) {
+		
 		this.brandPluginID = brandPluginID;
 		dataToDataGUIItemMap = new HashMap();
 		
