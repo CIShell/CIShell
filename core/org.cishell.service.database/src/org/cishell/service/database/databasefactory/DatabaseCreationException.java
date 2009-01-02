7c1ed3fc8ca378abeba3b5089c8198de32e86f90
mwlinnem <mwlinnem@138b4e90-db10-0410-b6da-c669c309eabc>
2009-01-02 17:57:02 +0000

    Initial commit.

---
+++ core/org.cishell.service.database/src/org/cishell/service/database/databasefactory/DatabaseCreationException.java
@@ -0,0 +1,25 @@
+package org.cishell.service.database.databasefactory;
+
+public class DatabaseCreationException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	//Constructors from Exception superclass
+	
+	public DatabaseCreationException() {
+		super();
+	}
+
+	public DatabaseCreationException(String arg0) {
+		super(arg0);
+	}
+
+	public DatabaseCreationException(Throwable arg0) {
+		super(arg0);
+	}
+
+	public DatabaseCreationException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+}
