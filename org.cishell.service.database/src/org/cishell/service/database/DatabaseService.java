920f1c6cc9384054e96fb45002e07a56e4a18736
mwlinnem <mwlinnem@138b4e90-db10-0410-b6da-c669c309eabc>
2009-01-15 21:41:50 +0000

    Added createDatabase method that utilizes a ResultSet. Hopefully a good idea to do it this way.

--- org.cishell.service.database/src/org/cishell/service/database/DatabaseService.java
+++ org.cishell.service.database/src/org/cishell/service/database/DatabaseService.java
@@ -1,9 +1,12 @@
 package org.cishell.service.database;
 
+import java.sql.ResultSet;
+
 import javax.sql.DataSource;
 
 public interface DatabaseService {
 
 	public DataSource createDatabase() throws DatabaseCreationException;
+	public DataSource createDatabase(ResultSet resultSet) throws DatabaseCreationException;
 	public DataSource copyDatabase(DataSource database) throws DatabaseCopyException;
 }
