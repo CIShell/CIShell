a887337bc84b54abe4965a796a1e6a04ab740083
mwlinnem <mwlinnem@138b4e90-db10-0410-b6da-c669c309eabc>
2009-01-15 21:36:27 +0000

    Replacing with a non-botched plugin setup.

--- org.cishell.service.database/src/org/cishell/service/database/DatabaseService.java
+++
@@ -1,9 +0,0 @@
-package org.cishell.service.database;
-
-import javax.sql.DataSource;
-
-public interface DatabaseService {
-
-	public DataSource createDatabase() throws DatabaseCreationException;
-	public DataSource copyDatabase(DataSource database) throws DatabaseCopyException;
-}
