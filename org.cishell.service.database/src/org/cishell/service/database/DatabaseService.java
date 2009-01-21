5b521d38bd673168acbfd0ec5dc119d8377d5e44
pataphil <pataphil@138b4e90-db10-0410-b6da-c669c309eabc>
2009-01-21 21:09:43 +0000

    Updated database service interface to not include methods it doesn't need/use.

--- org.cishell.service.database/src/org/cishell/service/database/DatabaseService.java
+++ org.cishell.service.database/src/org/cishell/service/database/DatabaseService.java
@@ -1,12 +1,5 @@
 package org.cishell.service.database;
 
-import java.sql.ResultSet;
-
-import javax.sql.DataSource;
-
 public interface DatabaseService {
-
 	public DataSourceWithID createDatabase() throws DatabaseCreationException;
-	public DataSourceWithID createDatabase(ResultSet resultSet) throws DatabaseCreationException;
-	public DataSourceWithID copyDatabase(DataSourceWithID database) throws DatabaseCopyException;
 }
