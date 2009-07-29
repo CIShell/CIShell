b6d8f614598219437a77b8a31204440399dd09a1
mwlinnem <mwlinnem@138b4e90-db10-0410-b6da-c669c309eabc>
2009-07-29 21:33:47 +0000

    Updated converter tester to conform to CIShell 1.0 changes (finally).

--- testing/org.cishell.testing.convertertester.algorithm/src/org/cishell/testing/convertertester/algorithm/ConverterTesterAlgorithmUtil.java
+++ testing/org.cishell.testing.convertertester.algorithm/src/org/cishell/testing/convertertester/algorithm/ConverterTesterAlgorithmUtil.java
@@ -8,21 +8,25 @@ import org.osgi.framework.ServiceReference;
 import org.osgi.service.log.LogService;
 
 public class ConverterTesterAlgorithmUtil implements AlgorithmProperty {
-	public static ServiceReference[] getConverterReferences(
-			BundleContext bContext) {
-		  String filter = "(&("+ALGORITHM_TYPE+"="+TYPE_CONVERTER+"))";
+	/*
+	 * Technically, this returns both converters AND validators.
+	 * However, when it was written validators were converters!
+	 */
+	public static ServiceReference[] getConverterReferences(BundleContext bContext) {
+		String filter = "(|(" + ALGORITHM_TYPE + "=" + TYPE_CONVERTER + ")(" + ALGORITHM_TYPE + "="
+				+ TYPE_VALIDATOR + "))";
 
-		  try {
-		  ServiceReference[] refs = bContext.getServiceReferences(
-				  AlgorithmFactory.class.getName(), filter);
-		  
-		  return refs;
-		  } catch (InvalidSyntaxException e) {
-			  System.err.println("Invalid syntax '" + filter +
-					  "' for filtering service references. Attempted to " +
-					  "obtain all converter references.");
-			  e.printStackTrace();
-			  return null;
-		  }
+		try {
+			ServiceReference[] refs = bContext.getServiceReferences(AlgorithmFactory.class
+					.getName(), filter);
+
+			return refs;
+		} catch (InvalidSyntaxException e) {
+			System.err.println("Invalid syntax '" + filter
+					+ "' for filtering service references. Attempted to "
+					+ "obtain all converter references.");
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
