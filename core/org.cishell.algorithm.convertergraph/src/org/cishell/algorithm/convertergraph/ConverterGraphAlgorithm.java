cf8faa56b373c1955ebee298463b767944c5881a
tankchintan <tankchintan@138b4e90-db10-0410-b6da-c669c309eabc>
2009-07-30 16:57:02 +0000

    Converted the plugin to use jung/graph instead of nwb to resolve the dependency issue, since converter graph is inside the org.cishell package.

--- core/org.cishell.algorithm.convertergraph/src/org/cishell/algorithm/convertergraph/ConverterGraphAlgorithm.java
+++ core/org.cishell.algorithm.convertergraph/src/org/cishell/algorithm/convertergraph/ConverterGraphAlgorithm.java
@@ -1,13 +1,6 @@
 package org.cishell.algorithm.convertergraph;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import org.cishell.framework.CIShellContext;
 import org.cishell.framework.algorithm.Algorithm;
@@ -21,9 +14,11 @@ import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.log.LogService;
 
+import edu.uci.ics.jung.graph.Graph;
+
 /**
  * This plugin collects information about all the active converters in the tool and 
- * outputs a NWB file having this Directed network. Also nodes corresponding to each
+ * outputs a JUNG Graph having this Directed network. Also nodes corresponding to each
  * MIME/TYPE are weighted depending upon how many times they participate in a converter
  * relationship.
  *  
@@ -35,7 +30,9 @@ public class ConverterGraphAlgorithm implements Algorithm {
 	private LogService logger;
 	private BundleContext bundleContext;
 	
-	private int nodeCount, edgeCount;
+	private Graph outputGraph;
+	
+	private int nodeCount = 0, edgeCount = 0;
 
     /**
      * Construct with the appropriate parameters.
@@ -51,122 +48,24 @@ public class ConverterGraphAlgorithm implements Algorithm {
 
     public Data[] execute() throws AlgorithmExecutionException {
 		
-			try {
-				
-				/*
-				 * Get all the converter service references currently in service.
-				 * */
-				ServiceReference[] allConverterServices = getAllConverters();
-				
-				/*
-				 * Process the references to create a network of weighted nodes & directed edges.
-				 * */
-				ConverterGraphComputation converterGraphComputation = 
-					new ConverterGraphComputation(allConverterServices, logger);
-				
-				/*
-				 * Used to generate the output file containing the network. 
-				 * */
-				File outputNWBFile = createOutputGraphFile(converterGraphComputation);
-				
-				return prepareOutputMetadata(new BasicData(outputNWBFile, "file:text/nwb"));
+			/*
+			 * Get all the converter service references currently in service.
+			 * */
+			ServiceReference[] allConverterServices = getAllConverters();
 			
-			} catch (IOException e) {
-				throw new AlgorithmExecutionException(e);
-			} 
+			/*
+			 * Process the references to create a network of weighted nodes & directed edges.
+			 * */
+			ConverterGraphComputation converterGraphComputation = 
+				new ConverterGraphComputation(allConverterServices, logger);
+			
+			/*
+			 * Used to generate the output reference for graph containing the network. 
+			 * */
+			return prepareOutputMetadata(new BasicData(converterGraphComputation.getOutputGraph(), 
+										 Graph.class.getName())); 
 	}
 
-	/**
-	 * Call all the NWB File Handler processes in order to create a NWB file.
-	 * @param converterGraphComputation
-	 * @return
-	 * @throws IOException
-	 */
-	private File createOutputGraphFile(
-			ConverterGraphComputation converterGraphComputation)
-			throws IOException {
-		
-		File outputNWBFile = File.createTempFile("nwb-", ".nwb");
-		
-		ConverterGraphOutputGenerator outputGenerator = new ConverterGraphOutputGenerator(
-				converterGraphComputation, outputNWBFile);
-		
-		outputGenerator.addComment("Graph of all Converters in the Tool.");
-		
-		nodeCount = converterGraphComputation.nodes.size();
-		outputGenerator.setNodeCount(nodeCount);
-		
-		outputGenerator.setNodeSchema(converterGraphComputation.nodeSchema);
-		
-		/*
-		 * Print all the node rows to the output file.
-		 * */
-		setNodeTuples(converterGraphComputation, outputGenerator);
-		
-		edgeCount = converterGraphComputation.edges.size();
-		outputGenerator.setDirectedEdgeCount(edgeCount);
-		
-		outputGenerator.setDirectedEdgeSchema(converterGraphComputation.edgeSchema);
-		
-		/*
-		 * Print all the edge rows to the output file. 
-		 * */
-		setDirectedEdgeTuples(converterGraphComputation, outputGenerator);
-		
-		outputGenerator.finishedParsing();
-		outputGenerator.haltParsingNow();
-		return outputNWBFile;
-	}
-
-	/**
-	 * Iterate through the nodes and print it into the output file. 
-	 * @param converterGraphComputation
-	 * @param outputGenerator
-	 */
-	private void setDirectedEdgeTuples(
-			ConverterGraphComputation converterGraphComputation,
-			ConverterGraphOutputGenerator outputGenerator) {
-		
-		for (Iterator edgeIterator = converterGraphComputation.edges.iterator(); 
-				edgeIterator.hasNext();) {
-			
-			Edge edge = (Edge) edgeIterator.next();
-			int sourceNode = edge.source;
-			int targetNode = edge.target;
-			final String converterName = edge.serviceShortPID;
-			final String servicePID = edge.serviceCompletePID;
-				
-			outputGenerator.addDirectedEdge(sourceNode, targetNode, new HashMap() {{
-				put("converter_name", converterName);
-				put("service_pid", servicePID);
-			}});
-		}
-	}
-
-	/**
-	 * @param converterGraphComputation
-	 * @param outputGenerator
-	 */
-	private void setNodeTuples(
-			ConverterGraphComputation converterGraphComputation,
-			ConverterGraphOutputGenerator outputGenerator) {
-		
-		for (Iterator nodeIterator = converterGraphComputation.nodes.entrySet().iterator(); 
-				nodeIterator.hasNext();) {
-			
-			Map.Entry node = (Entry) nodeIterator.next();
-			
-			int nodeID = ((Node) node.getValue()).id;
-			final int strength = ((Node) node.getValue()).strength;
-			String label = node.getKey().toString();
-				
-			outputGenerator.addNode(nodeID, label, new HashMap() {{
-				put("strength", strength);
-			}});
-		}
-	}
-    
-	
 	/**
 	 * Gets all the converter service references based on the LDAP query. 
 	 * @return
@@ -199,8 +98,10 @@ public class ConverterGraphAlgorithm implements Algorithm {
 	 * @param outNWBData
 	 */
 	private Data[] prepareOutputMetadata(Data outNWBData) {
+		
 		outNWBData.getMetadata().put(DataProperty.LABEL, "Converter Graph having " 
-				+ nodeCount + " nodes & " + edgeCount + " edges.");
+				+ ((Graph) outNWBData.getData()).numVertices() + " nodes & " 
+				+ ((Graph) outNWBData.getData()).numEdges() + " edges.");
 		outNWBData.getMetadata().put(DataProperty.TYPE, DataProperty.NETWORK_TYPE);
 		return new Data[]{outNWBData};
 	}
--- core/org.cishell.algorithm.convertergraph/src/org/cishell/algorithm/convertergraph/Edge.java
+++
@@ -1,8 +0,0 @@
-package org.cishell.algorithm.convertergraph;
-
-public class Edge {
-	public int source;
-	public int target;
-	public String serviceShortPID;
-	public String serviceCompletePID;
-}
