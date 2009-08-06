174eaa6ab8200de38c38b90d88502b694d8e7578
jrbibers <jrbibers@138b4e90-db10-0410-b6da-c669c309eabc>
2009-08-06 22:02:26 +0000

    Added CollectionUtilities to CIShell utilities.  Initial commit includes only a method which filters a Collection according to a Dictionary into Boolean values.

---
+++ core/org.cishell.utilities/src/org/cishell/utilities/CollectionUtilities.java
@@ -0,0 +1,31 @@
+package org.cishell.utilities;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Dictionary;
+import java.util.Iterator;
+
+public class CollectionUtilities {
+
+	/* Return only elements of the Collection which are mapped to true in the
+	 * Dictionary
+	 */
+	public static Collection grabSelectedValues(
+			Collection elements, Dictionary selectionDictionary) {
+		Collection selectedElements = new ArrayList();
+	
+		for (Iterator elementsIt = elements.iterator(); elementsIt.hasNext();) {
+			String element = (String) elementsIt.next();
+			Object isSelected = selectionDictionary.get(element);
+	
+			if ((isSelected != null) && (isSelected instanceof Boolean)) {
+				if (((Boolean) isSelected).booleanValue()) {
+					selectedElements.add(element);
+				}
+			}
+		}
+	
+		return selectedElements;
+	}
+
+}
