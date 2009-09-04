910c1c131602f1db5e4e26d9072ebee2c4e45165
jrbibers <jrbibers@138b4e90-db10-0410-b6da-c669c309eabc>
2009-09-04 21:08:26 +0000

    In the colored-region style, we now report when some region was requested to be painted a particular color, but no region of that name could be found in the shapefile.  Previously, if a user gave the algorithm a table with a row specifying a country named "Russian Federation", there would be no warning that that data wasn't represented in the output map (as our current world countries shapefile has a "Russia" feature, but no "Russian Federation" feature).  If any such failures occur, the user is now told how many occurred, the NWB user console shows at least a few of them, and the complete list is written to the log file.
    Added two new implosion utilities in support of this.

--- core/org.cishell.utilities/src/org/cishell/utilities/StringUtilities.java
+++ core/org.cishell.utilities/src/org/cishell/utilities/StringUtilities.java
@@ -1,5 +1,7 @@
 package org.cishell.utilities;
 
+import java.util.List;
+
 public class StringUtilities {
 	public static String implodeStringArray(String[] stringArray, String separator) {
 		final int stringArrayLength = stringArray.length;
@@ -8,6 +10,23 @@ public class StringUtilities {
 		for (int ii = 0; ii < stringArrayLength; ii++) {
 			workingResultString.append(stringArray[ii]);
 			if (ii != stringArrayLength - 1) {
+				workingResultString.append(separator);
+			}
+		}
+		
+		return workingResultString.toString();
+	}
+	
+	public static String implodeList(List list, String separator) {
+		StringBuffer workingResultString = new StringBuffer();
+		
+		final int listLength = list.size();
+		
+		for (int ii = 0; ii < listLength; ii++) {
+			workingResultString.append(list.get(ii));
+			
+			boolean isLastElement = (ii == listLength - 1);
+			if (!isLastElement) {
 				workingResultString.append(separator);
 			}
 		}
