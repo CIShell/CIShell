3c8efc272594a82f402ec607b64c709fec48619e
jrbibers <jrbibers@138b4e90-db10-0410-b6da-c669c309eabc>
2009-10-13 17:57:55 +0000

    Corrected a design flaw in the mutateParameter utilities.
    To date, all parameters transformed using ObjectClassDefinitionTransformer have been uniformly written out with the attribute definition filter ObjectClassDefinition.REQUIRED.  Now the transformer preserves each AttributeDefinition's original filter (currently one of ObjectClassDefinition.REQUIRED or ObjectClassDefinition.OPTIONAL, where this assumption is reflected in the constant ATOMIC_ATTRIBUTE_DEFINITION_FILTERS).
    
    This change should not alter the behavior of any plug-in currently depending on the mutateParameter utilities (since none, to my knowledge, have optional parameters).

--- org.cishell.utilities/src/org/cishell/utilities/mutateParameter/ObjectClassDefinitionTransformer.java
+++ org.cishell.utilities/src/org/cishell/utilities/mutateParameter/ObjectClassDefinitionTransformer.java
@@ -1,5 +1,7 @@
 package org.cishell.utilities.mutateParameter;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -9,10 +11,20 @@ import org.osgi.service.metatype.AttributeDefinition;
 import org.osgi.service.metatype.ObjectClassDefinition;
 
 public class ObjectClassDefinitionTransformer {
-	private static final int OUTGOING_ATTRIBUTES_FILTER =
-		ObjectClassDefinition.REQUIRED;
-	private static final int INCOMING_ATTRIBUTES_FILTER =
-		ObjectClassDefinition.ALL;
+	/**
+	 * AttributeDefinition filters as described in ObjectClassDefinition.
+	 * "Atomic" to exclude blanket filters like ObjectClassDefinition.ALL.
+	 * @see ObjectClassDefinition#REQUIRED
+	 * @see ObjectClassDefinition#OPTIONAL
+	 * @see ObjectClassDefinition#ALL
+	 */
+	public static final List ATOMIC_ATTRIBUTE_DEFINITION_FILTERS;
+	static {
+		List l = new ArrayList();
+		l.add(ObjectClassDefinition.REQUIRED);
+		l.add(ObjectClassDefinition.OPTIONAL);
+		ATOMIC_ATTRIBUTE_DEFINITION_FILTERS = Collections.unmodifiableList(l);
+	}
 
 	/* Create newOCD from oldOCD by applying transformer
 	 * to each AttributeDefinition.
@@ -23,12 +35,20 @@ public class ObjectClassDefinitionTransformer {
 		BasicObjectClassDefinition newOCD =
 			MutateParameterUtilities.createNewParameters(oldOCD);
 		
-		AttributeDefinition[] oldADs =
-			oldOCD.getAttributeDefinitions(INCOMING_ATTRIBUTES_FILTER);
-		for (int ii = 0; ii < oldADs.length; ii++) {
-			newOCD.addAttributeDefinition(
-					OUTGOING_ATTRIBUTES_FILTER,
-					transformer.transform(oldADs[ii]));
+		// For each kind of AttributeDefinition filter ..
+		for (Iterator filterIt = ATOMIC_ATTRIBUTE_DEFINITION_FILTERS.iterator();
+				filterIt.hasNext();) {
+			int filter = ((Integer) filterIt.next()).intValue();
+			
+			// Grab all matching AttributeDefinitions and transform them.
+			AttributeDefinition[] oldADs =
+				oldOCD.getAttributeDefinitions(filter);
+			
+			for (int ii = 0; ii < oldADs.length; ii++) {
+				newOCD.addAttributeDefinition(
+							filter,
+							transformer.transform(oldADs[ii]));
+			}
 		}
 		
 		return newOCD;
@@ -42,6 +62,7 @@ public class ObjectClassDefinitionTransformer {
 		for (Iterator it = transformers.iterator(); it.hasNext();) {
 			AttributeDefinitionTransformer transformer =
 				(AttributeDefinitionTransformer) it.next();
+			
 			newOCD = apply(transformer, newOCD);
 		}
 		
