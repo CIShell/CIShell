b0d76642274abee87397e55bd889764eab70a83e
fugu13 <fugu13@138b4e90-db10-0410-b6da-c669c309eabc>
2010-01-07 20:05:55 +0000

    Add some database utilities. Typical usage will be like so:
    execute() {
     //get out database
    
     Connection connection = DatabaseUtilities.connect(database);
    
     try {
     	//do stuff
     } catch (SQLException e) {
    
     } catch(FooException e) {
    
     } finally {
     	DatabaseUtilities.closeConnectionQuietly(connection);
     }
    
    }

---
+++ core/org.cishell.utilities/src/org/cishell/utilities/DatabaseUtilities.java
@@ -0,0 +1,28 @@
+package org.cishell.utilities;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.cishell.framework.algorithm.AlgorithmExecutionException;
+import org.cishell.service.database.Database;
+
+public class DatabaseUtilities {
+	public static void closeConnectionQuietly(Connection connection) {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			//quietly! Use this only in finally blocks, pretty much, and right before throwing exceptions that will leave the scope of the Connection object.
+		}
+	}
+
+	public static Connection connect(Database database)
+	throws AlgorithmExecutionException {
+		Connection connection;
+		try {
+			connection = database.getConnection();
+		} catch (SQLException e) {
+			throw new AlgorithmExecutionException("Unable to communicate with the database.", e);
+		}
+		return connection;
+	}
+}
