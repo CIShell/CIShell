a5b06cac1918e592c89ef347dbfd25103dd5df7f
fugu13 <fugu13@138b4e90-db10-0410-b6da-c669c309eabc>
2010-01-21 20:31:28 +0000

    Add various database utilities.

--- core/org.cishell.utilities/src/org/cishell/utilities/DatabaseUtilities.java
+++ core/org.cishell.utilities/src/org/cishell/utilities/DatabaseUtilities.java
@@ -2,12 +2,18 @@ package org.cishell.utilities;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.cishell.framework.algorithm.AlgorithmExecutionException;
 import org.cishell.service.database.Database;
 
 public class DatabaseUtilities {
 	public static void closeConnectionQuietly(Connection connection) {
+		if(connection == null) {
+			return;
+		}
 		try {
 			connection.close();
 		} catch (SQLException e) {
@@ -24,5 +30,39 @@ public class DatabaseUtilities {
 			throw new AlgorithmExecutionException(messageIfError, e);
 		}
 		return connection;
+	}
+	
+	public static String getInExpression(List<String> columns, List<Map<String, Object>> valueMaps) {
+		String columnNames = implodeAndWrap(columns);
+		
+		List<String> values = new ArrayList<String>();
+		for(Map<String, Object> oldValues : valueMaps) {
+			List<String> rowValues = new ArrayList<String>();
+			for(String column : columns) {
+				rowValues.add(formatValue(oldValues.get(column)));
+			}
+			values.add(implodeAndWrap(rowValues));
+		}
+		String columnValues = implodeAndWrap(values);
+		
+		
+		//first make part with column names
+		//then make part with groups of column values
+		return columnNames + " IN " + columnValues;
+	}
+	
+	//TODO: expand this to other sorts of SQL datatypes (per their Prefuse table equivalents)
+	public static String formatValue(Object value) {
+		if(value == null) {
+			return "NULL";
+		} else if(value instanceof Number) {
+			return value.toString();
+		} else {
+			return "'" + value.toString() + "'";
+		}
+	}
+	
+	public static String implodeAndWrap(List<String> values) {
+		return "(" + StringUtilities.implodeList(values, ", ") + ")";
 	}
 }
---
+++ core/org.cishell.utilities/src/org/cishell/utilities/database/Column.java
@@ -0,0 +1,55 @@
+package org.cishell.utilities.database;
+
+import java.sql.Types;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Column {
+	
+	public final String name;
+	public final int type;
+	public final int size;
+	
+	public static final Map<Integer, String> TYPE_MAP  = constructTypeMap();
+	public static final Set<Integer> SIZED_TYPES = constructSizedTypes();
+	
+	private static Map<Integer, String> constructTypeMap() { //if this ever gets derby specific, it shouldn't go here
+		Map<Integer, String> typeMap = new HashMap<Integer, String>() {{
+			put(Types.CHAR, "char");
+			put(Types.DATE, "date");
+			put(Types.DOUBLE, "double");
+			put(Types.FLOAT, "float");
+			put(Types.INTEGER, "integer");
+			put(Types.SMALLINT, "smallint");
+			put(Types.TIME, "time");
+			put(Types.VARCHAR, "varchar");
+		}};
+		return Collections.unmodifiableMap(typeMap);
+	}
+	
+	private static Set<Integer> constructSizedTypes() {
+		Set<Integer> sizedTypes = new HashSet<Integer>() {{
+			add(Types.CHAR);
+			add(Types.VARCHAR);
+			
+		}};
+		return null;
+	}
+
+	public Column(String name, int type, int size) {
+		this.name = name;
+		this.type = type;
+		this.size = size;
+	}
+
+	
+
+	public String getDefinition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
