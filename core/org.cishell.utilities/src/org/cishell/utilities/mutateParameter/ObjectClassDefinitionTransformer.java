55b531d5ea102781dd285365d4e351e86c527451
pataphil <pataphil@138b4e90-db10-0410-b6da-c669c309eabc>
2010-07-06 20:08:57 +0000

    * Partially made Java 1.5.

--- core/org.cishell.utilities/src/org/cishell/utilities/mutateParameter/ObjectClassDefinitionTransformer.java
+++ core/org.cishell.utilities/src/org/cishell/utilities/mutateParameter/ObjectClassDefinitionTransformer.java
@@ -1,13 +1,13 @@
 package org.cishell.utilities.mutateParameter;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
 
 import org.cishell.reference.service.metatype.BasicObjectClassDefinition;
 import org.cishell.utilities.MutateParameterUtilities;
+import org.cishell.utilities.mutateParameter.dropdown.DropdownTransformer;
 import org.osgi.service.metatype.AttributeDefinition;
 import org.osgi.service.metatype.ObjectClassDefinition;
 
@@ -18,37 +18,40 @@ public class ObjectClassDefinitionTransformer {
 	 * @see ObjectClassDefinition#REQUIRED
 	 * @see ObjectClassDefinition#OPTIONAL
 	 */
-	public static final List ATOMIC_ATTRIBUTE_DEFINITION_FILTERS;
-	static {
-		List l = new ArrayList();
-		l.add(new Integer(ObjectClassDefinition.REQUIRED));
-		l.add(new Integer(ObjectClassDefinition.OPTIONAL));
-		ATOMIC_ATTRIBUTE_DEFINITION_FILTERS = Collections.unmodifiableList(l);
-	}
+	public static final Collection<Integer> ATOMIC_ATTRIBUTE_DEFINITION_FILTERS =
+		Collections.unmodifiableList(Arrays.asList(
+			new Integer(ObjectClassDefinition.REQUIRED),
+			new Integer(ObjectClassDefinition.OPTIONAL)));
+//	static {
+//		List l = new ArrayList();
+//		l.add(new Integer(ObjectClassDefinition.REQUIRED));
+//		l.add(new Integer(ObjectClassDefinition.OPTIONAL));
+//		ATOMIC_ATTRIBUTE_DEFINITION_FILTERS = Collections.unmodifiableList(l);
+//	}
 
 	/* Create newOCD from oldOCD by applying transformer
 	 * to each AttributeDefinition.
 	 */
 	public static BasicObjectClassDefinition apply(
 			AttributeDefinitionTransformer transformer,
-			ObjectClassDefinition oldOCD,
+			ObjectClassDefinition oldObjectClassDefinition,
 			Collection<String> attributesToIgnore) {		
 		BasicObjectClassDefinition newOCD =
-			MutateParameterUtilities.createNewParameters(oldOCD);
+			MutateParameterUtilities.createNewParameters(oldObjectClassDefinition);
 		
 		// For each kind of AttributeDefinition filter ..
-		for (Iterator filterIt = ATOMIC_ATTRIBUTE_DEFINITION_FILTERS.iterator();
+		for (Iterator<Integer> filterIt = ATOMIC_ATTRIBUTE_DEFINITION_FILTERS.iterator();
 				filterIt.hasNext();) {
-			int filter = ((Integer) filterIt.next()).intValue();
+			int filter = filterIt.next().intValue();
 			
 			// Grab all matching AttributeDefinitions and transform them.
-			AttributeDefinition[] oldADs =
-				oldOCD.getAttributeDefinitions(filter);
-			
-			for (int ii = 0; ii < oldADs.length; ii++) {
-				if (!attributesToIgnore.contains(oldADs[ii].getID())) {
+			AttributeDefinition[] oldAttributeDefintions =
+				oldObjectClassDefinition.getAttributeDefinitions(filter);
+
+			for (AttributeDefinition attributeDefinition : oldAttributeDefintions) {
+				if (!attributesToIgnore.contains(attributeDefinition.getID())) {
 					newOCD.addAttributeDefinition(
-						filter, transformer.transform(oldADs[ii]));
+						filter, transformer.transform(attributeDefinition));
 				}
 			}
 		}
@@ -58,16 +61,17 @@ public class ObjectClassDefinitionTransformer {
 	
 	// Convenience method for batching transformations.
 	public static ObjectClassDefinition transform(
-			ObjectClassDefinition ocd, List transformers, Collection<String> attributesToIgnore) {
-		ObjectClassDefinition newOCD = ocd;
+			ObjectClassDefinition objectClassDefinition,
+			Collection<DropdownTransformer> transformers,
+			Collection<String> attributesToIgnore) {
+		ObjectClassDefinition newObjectClassDefinition = objectClassDefinition;
 		
-		for (Iterator it = transformers.iterator(); it.hasNext();) {
-			AttributeDefinitionTransformer transformer =
-				(AttributeDefinitionTransformer) it.next();
-			
-			newOCD = apply(transformer, newOCD, attributesToIgnore);
+		for (Iterator<DropdownTransformer> it = transformers.iterator(); it.hasNext();) {
+			DropdownTransformer transformer = it.next();
+			newObjectClassDefinition =
+				apply(transformer, newObjectClassDefinition, attributesToIgnore);
 		}
 		
-		return newOCD;
+		return newObjectClassDefinition;
 	}
 }
