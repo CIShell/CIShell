e91866a1f9ba50ba382f39eb27cf8388df09da91
pataphil <pataphil@138b4e90-db10-0410-b6da-c669c309eabc>
2010-07-21 03:50:35 +0000

    * Added GUIBuilderUtilities.setCancelable().

---
+++ core/org.cishell.utilities/src/org/cishell/utilities/ObjectContainer.java
@@ -0,0 +1,12 @@
+package org.cishell.utilities;
+
+public class ObjectContainer<T> {
+	public T object;
+
+	public ObjectContainer() {
+	}
+
+	public ObjectContainer(T object) {
+		this.object = object;
+	}
+};
