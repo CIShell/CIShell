580e241b1fe5a6b465c6d7ae49fbb1df3ffe3bcd
pataphil <pataphil@138b4e90-db10-0410-b6da-c669c309eabc>
2010-08-03 17:21:00 +0000

    * Moved SWT Utilities out of org.cishell.utilities bundle.

---
+++ core/org.cishell.utilities.swt/src/org/cishell/utilities/swt/model/ModelFieldException.java
@@ -0,0 +1,21 @@
+package org.cishell.utilities.swt.model;
+
+public class ModelFieldException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ModelFieldException() {
+		super();
+	}
+
+	public ModelFieldException(String arg0) {
+		super(arg0);
+	}
+
+	public ModelFieldException(Throwable arg0) {
+		super(arg0);
+	}
+
+	public ModelFieldException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+}
