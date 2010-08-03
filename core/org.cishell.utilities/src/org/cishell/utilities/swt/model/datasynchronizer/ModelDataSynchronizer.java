24e29b9a5a21d1806eacbe2d54c84bfa4fe0e00d
pataphil <pataphil@138b4e90-db10-0410-b6da-c669c309eabc>
2010-08-03 17:23:41 +0000

    * Moved SWT Utilities out to org.cishell.utilities.swt bundle.

--- core/org.cishell.utilities/src/org/cishell/utilities/swt/model/datasynchronizer/ModelDataSynchronizer.java
+++
@@ -1,9 +0,0 @@
-package org.cishell.utilities.swt.model.datasynchronizer;
-
-public interface ModelDataSynchronizer<T> {
-	public int swtUpdateListenerCode();
-	public T value();
-	public T synchronizeFromGUI();
-	public T synchronizeToGUI(T value);
-	public T reset(T defaultValue);
-}
