c81b3299d4a60e4c2fe29adb52d0ce1f64b2f0d1
pataphil <pataphil@138b4e90-db10-0410-b6da-c669c309eabc>
2010-08-26 17:58:18 +0000

    * Restructured how validation works. * Not reviewed.

--- org.cishell.utility.datastructure/src/org/cishell/utility/datastructure/datamodel/field/validation/FieldValidationRule.java
+++ org.cishell.utility.datastructure/src/org/cishell/utility/datastructure/datamodel/field/validation/FieldValidationRule.java
@@ -1,10 +1,16 @@
-package org.cishell.utility.datastructure.datamodel.field;
+package org.cishell.utility.datastructure.datamodel.field.validation;
+
+import java.util.Collection;
 
 import org.cishell.utility.datastructure.datamodel.DataModel;
 import org.cishell.utility.datastructure.datamodel.exception.ModelValidationException;
+import org.cishell.utility.datastructure.datamodel.field.DataModelField;
 
 public interface FieldValidationRule<ValueType> {
 	public void validateField(DataModelField<ValueType> field, DataModel model)
 			throws ModelValidationException;
+	/// Update any internal states as necessary.
+	public void fieldUpdated(DataModelField<ValueType> field);
+	public void fieldsUpdated(Collection<DataModelField<ValueType>> fields);
 	public void fieldDisposed(DataModelField<ValueType> field);
 }
