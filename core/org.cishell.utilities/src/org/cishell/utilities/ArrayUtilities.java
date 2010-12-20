73730daf89dc3fa377d9d8d8a77fe0d1a573b585
kongchinhua <kongchinhua@138b4e90-db10-0410-b6da-c669c309eabc>
2010-12-20 22:16:06 +0000

    - Add ArrayUtilities.copyOf() to support expend array while copy - Use Arrays.copyOf instead of ArrayUtilities.copyOf due to JRE1.5 doesn't support Arrays.copyOf - Fixed style guide warnings in ArrayUtilities.java - Reviewed by Joseph

--- core/org.cishell.utilities/src/org/cishell/utilities/ArrayUtilities.java
+++ core/org.cishell.utilities/src/org/cishell/utilities/ArrayUtilities.java
@@ -3,7 +3,11 @@ package org.cishell.utilities;
 import java.util.Iterator;
 import java.util.List;
 
-public class ArrayUtilities {
+public final class ArrayUtilities {
+	
+	private ArrayUtilities() {
+	}
+	
 	public static int indexOf(Object[] array, Object target) {
 		for (int ii = 0; ii < array.length; ii++) {
 			if (array[ii].equals(target)) {
@@ -14,12 +18,12 @@ public class ArrayUtilities {
 		return -1;
 	}
 	
-	public static void swapFirstMatchToFront(Object[] array, List targets) {
-		for (Iterator targetsIt = targets.iterator(); targetsIt.hasNext();) {
+	public static void swapFirstMatchToFront(Object[] array, List<?> targets) {
+		for (Iterator<?> targetsIt = targets.iterator(); targetsIt.hasNext();) {
 			Object target = targetsIt.next();
 			int index = ArrayUtilities.indexOf(array, target);
 			
-			if ( index != -1 ) {
+			if (index != -1) {
 				swap(array, 0, index);
 				return;
 			}
@@ -41,6 +45,31 @@ public class ArrayUtilities {
 		}
 		
 		return clone;
+	}
+	
+	/**
+	 * This method will return a shadow copy of the given array with the desired size.
+	 * If the given disiredSize exceeds the original array size, the remaining items
+	 * will be filled with empty string.
+	 * @param array - The original array to be copied
+	 * @param desiredSize - The desired size of the new array
+	 * @return The new copy of the original array with the desired size
+	 */
+	public static String[] copyOf(String[] array, int desiredSize) {
+		String[] newArray = new String[desiredSize];
+		int copiedSize = Math.min(array.length, desiredSize);
+		
+		/* Copy items from the original array to new array */
+		for (int i = 0; i < copiedSize; i++) {
+			newArray[i] = array[i];
+		}
+		
+		/* Fill the extra items will empty string if there are */
+		for (int i = copiedSize; i < desiredSize; i++) {
+			newArray[i] = "";
+		}
+		
+		return newArray;
 	}
 
 	// TODO: Find a better place to put this?
