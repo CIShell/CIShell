bba619930127ba3895eba9427250c3a32e1e4e31
pataphil <pataphil@138b4e90-db10-0410-b6da-c669c309eabc>
2011-01-07 18:41:48 +0000

    * http://cns-jira.slis.indiana.edu/browse/SCISQUARED-360 * Also cleaned up code a little, as I read through it. * Reviewed by Joseph.

--- clients/gui/org.cishell.reference.gui.persistence/src/org/cishell/reference/gui/persistence/view/FileViewFactory.java
+++ clients/gui/org.cishell.reference.gui.persistence/src/org/cishell/reference/gui/persistence/view/FileViewFactory.java
@@ -6,12 +6,18 @@ import org.cishell.framework.CIShellContext;
 import org.cishell.framework.algorithm.Algorithm;
 import org.cishell.framework.algorithm.AlgorithmFactory;
 import org.cishell.framework.data.Data;
+import org.cishell.service.conversion.DataConversionService;
+import org.osgi.service.log.LogService;
 
 
 public class FileViewFactory implements AlgorithmFactory {
-	public Algorithm createAlgorithm(Data[] data,
-									 Dictionary parameters,
-									 CIShellContext ciShellContext) {
-        return new FileView(data, parameters, ciShellContext);
+	public Algorithm createAlgorithm(
+			Data[] data, Dictionary<String, Object> parameters, CIShellContext ciShellContext) {
+		DataConversionService conversionManager =
+			(DataConversionService) ciShellContext.getService(
+				DataConversionService.class.getName());
+		LogService logger = (LogService) ciShellContext.getService(LogService.class.getName());
+
+        return new FileView(data, ciShellContext, conversionManager, logger);
     }
 }
