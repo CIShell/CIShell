743a046353f1ac6017667132cca1ca8bb5f35ba8
jrbibers <jrbibers@138b4e90-db10-0410-b6da-c669c309eabc>
2011-11-21 17:59:11 +0000

    New class org.cishell.utilities.DataFactory with static factory methods for creating Data instances.  This should reduce the need for those pervasive 5-to-7 line "wrapWithMetadata"-type methods in almost every algorithm.
    Convenience methods for four common Data creation cases:
    * With prescribed format and type.
    * With a prescribed type, but format = {wrapped object}.getClass().getName().
    * Inheriting the parent's format and type.
    * For a file with prescribed MIME-type (format) and type.  This is just an alias for the first.
    
    Currently uses BasicData as the Data implementation, but this can be changed later.
    
    Reviewed by Thomas.

---
+++ core/org.cishell.utilities/src/org/cishell/utilities/DataFactory.java
@@ -0,0 +1,83 @@
+package org.cishell.utilities;
+
+import java.io.File;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.cishell.framework.data.BasicData;
+import org.cishell.framework.data.Data;
+import org.cishell.framework.data.DataProperty;
+
+public final class DataFactory {
+	private DataFactory() {
+		// Static factory methods only.
+	}
+	
+	/**
+     * Creates a Data wrapping {@code object}.
+     * 
+     * @param datum		Object to wrap
+     * @param format	The {@link Data#getFormat() format} of {@code object}
+     * @param type		The {@link DataProperty#TYPE type} of {@code object}
+	 * 					See {@link DataProperty}.*_TYPE for possible values
+     * @param parent	The {@link Data} from which {@code object} was derived
+     * @param label		A concise String describing {@code object} in relation to {@code parent}
+     */
+    public static Data forObject(Object object, String format, String type, Data parent, String label) {
+		Dictionary<String, Object> metadata = new Hashtable<String, Object>();
+		metadata.put(DataProperty.TYPE, type);
+		metadata.put(DataProperty.PARENT, parent);
+		metadata.put(DataProperty.LABEL, label);
+		
+		return new BasicData(metadata, object, format);
+	}
+    /**
+     * Creates a Data wrapping {@code object} having the same {@link Data#getFormat() format} and
+     * {@link DataProperty#TYPE type} as its {@code parent}.
+     * 
+     * @param object	Object to wrap
+     * @param parent	The {@link Data} from which {@code object} was derived
+     * @param label		A concise String describing {@code object} in relation to {@code parent}
+     * @return	A Data wrapping {@code object} using {@code parent}'s {@code format}
+     * 			and {@code type}
+     */
+	public static Data likeParent(Object object, Data parent, String label) {
+		return forObject(
+					object,
+					parent.getFormat(),
+					(String) parent.getMetadata().get(DataProperty.TYPE),
+					parent,
+					label);
+	}
+	/**
+	 * Creates a Data wrapping {@code object} using {@code object.getClass().getName()} as its
+	 * format.
+	 * 
+	 * @param object	Object to wrap as Data
+	 * @param type		The {@link DataProperty#TYPE type} of {@code object}
+	 * 					See {@link DataProperty}.*_TYPE for possible values
+	 * @param parent	The {@link Data} from which {@code object} was derived
+	 * @param label		A concise String describing {@code object} in relation to {@code parent}
+	 * @return	A Data wrapping {@code object} using {@code object.getClass().getName()} as its
+	 * 			format 
+	 */
+	public static Data withClassNameAsFormat(
+			Object object, String type, Data parent, String label) {
+		return forObject(object, object.getClass().getName(), type, parent, label);
+	}
+	/**
+	 * Creates a Data wrapping {@code file} using {@code mimeType} as its format.
+	 * 
+	 * @param file		File to wrap as Data
+	 * @param mimeType	{@code file}'s MIME type, e.g. "file:text/plain".
+	 * @param type		The {@link DataProperty#TYPE type} of {@code object}
+	 * 					See {@link DataProperty}.*_TYPE for possible values
+	 * @param parent	The {@link Data} from which {@code object} was derived
+	 * @param label		A concise String describing {@code object} in relation to {@code parent}
+	 * @return	A Data wrapping {@code file} using {@code mimeType} as its format
+	 */
+	public static Data forFile(
+			File file, String mimeType, String type, Data parent, String label) {
+		return forObject(file, mimeType, type, parent, label);
+	}
+}
