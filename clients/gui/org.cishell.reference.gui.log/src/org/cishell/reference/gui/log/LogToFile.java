5c3462a2725fd9debcbd7152c4e6e1bda50133b8
dmcoe <dmcoe@indiana.edu>
2012-05-08 16:48:17 -0400

    An update of the logtofile.
    Known Issues:
    Some debug messages are missed.

--- clients/gui/org.cishell.reference.gui.log/src/org/cishell/reference/gui/log/LogToFile.java
+++ clients/gui/org.cishell.reference.gui.log/src/org/cishell/reference/gui/log/LogToFile.java
@@ -11,139 +11,199 @@ import java.util.logging.SimpleFormatter;
 
 import org.osgi.service.log.LogEntry;
 import org.osgi.service.log.LogListener;
+import org.osgi.service.log.LogService;
 
 /**
  * This is a basic implementation. It writes log records to files
+ * 
  * @author Weixia(Bonnie) Huang (huangb@indiana.edu)
  * @author Felix Terkhorn (terkhorn@gmail.com)
+ * @author David M. Coe (david.coe@gmail.com)
  */
 public class LogToFile implements LogListener {
-	//default log directory
-	private static final String DEFAULT_LOG_DIR = 
-		System.getProperty("user.dir") 
-		+ File.separator + "logs";
-	private static final String FILE_NAME_PREFIX = "user";
-	
-	private File currentDir;
-	private Logger logger;
-	
-	//Specify the default size of each log file
+	private static final String NEWLINE = System.getProperty("line.separator");
+
+	// Specify file handler properties
 	private static final int LIMIT = 100000; // 100 kb
-	
-	private static final int MAX_NUM_LOG_FILES  = 10;
+	private static final int MAX_NUM_LOG_FILES = 10;
+	private static final boolean APPEND = true;
 
-    /**
-     * Constructor
-     */
-    public LogToFile() {
-    	
-    	try {
-            // Create an appending file handler
-    		currentDir = getLogDirectory();
-    		if (currentDir != null){
-    			boolean append = true;
-    			String logFileName = currentDir+File.separator+
-								generateUniqueFile(FILE_NAME_PREFIX)+
-								".%g.log";
-    			
-    			FileHandler handler = new FileHandler(logFileName, 
-            			LIMIT, MAX_NUM_LOG_FILES, append);
-    			
-    			handler.setFormatter(new SimpleFormatter());
-        
-    			// Add to the desired logger
-    			logger = Logger.getLogger("edu.iu.iv.logger");
-    			logger.addHandler(handler);
-    			logger.setUseParentHandlers(false); // Edited by Felix Terkhorn.  terkhorn@gmail.com May-9-2007
-    		}
-        } catch (IOException e) {
-        	e.printStackTrace();
-        }
+	// Specify whether or not this logger should send its output to its parent
+	// Logger
+	private static final boolean SEND_TO_PARENT_LOGGER = false;
 
-    }
-	
-    public void logged(final LogEntry entry) {
-    	String message = entry.getMessage();
-    	int osgiLogLevel = entry.getLevel();
-    	Level javaLogLevel;
-/*
-    	// Correspondence between OSGI log levels and Java.util.logging log levels:
-    	// JAVA				OSGI
-    	// ----				----
-    	// SEVERE=1000		LOG_ERROR=1
-    	// WARNING=900		LOG_WARNING=2
-    	// INFO=800			LOG_INFO=3
-    	// FINEST=300		LOG_DEBUG=4
-  */  	
-    	if (osgiLogLevel == 1) { // OSGI level = LOG_ERROR
-    		javaLogLevel = Level.SEVERE;
-    	} else if (osgiLogLevel == 2) {  // OSGI level = LOG_WARNING
-    		javaLogLevel = Level.WARNING;
-    	} else if (osgiLogLevel == 3) {  // OSGI level = LOG_INFO
-    		javaLogLevel = Level.INFO;
-    	} else if (osgiLogLevel == 4) {  // OSGI level = LOG_DEBUG
-    		javaLogLevel = Level.FINEST;
-    	} else { // if these don't match, set it to INFO
-    		     // maybe not the best way to do this
-    		javaLogLevel = Level.INFO;
-    	}  // edited by Felix Terkhorn.  terkhorn@gmail.com   May-9-2007
-    	
-    	if (goodMessage(message)) {
-    		// stdout printing happens here, despite having 1 handler only.
-    		logger.log(javaLogLevel, message + "\r\n", entry.getException());
-    	}
-    }
-    
-    private boolean goodMessage(String msg) {
-        if (msg == null || 
-                msg.startsWith("ServiceEvent ") || 
-                msg.startsWith("BundleEvent ") || 
-                msg.startsWith("FrameworkEvent ")) {
-            return false;
-        } else {
-            return true;   
-        }
-    }
+	private static final String LOGGER_NAME = "org.cishell.logger.file_logger";
 
-    private static File getLogDirectory(){    	
-    	//later, we should get the log directory from preference service
-    	File logDir = new File(DEFAULT_LOG_DIR);
-    	if (!logDir.exists() || !logDir.isDirectory()){  
-    		try{
-    			if (logDir.mkdir()){
-    				return logDir;
-    			} else {
-    				return new File (DEFAULT_LOG_DIR);
-    			}
-    		}catch (Exception e){
-    			e.printStackTrace();
-    			return new File (DEFAULT_LOG_DIR);
-    		}
-    		
-    	}else
-    		return logDir;		
-            
-    }
-    
-    /*
-     * create log file with given name plus unique timestamp
-     */
-    private String generateUniqueFile(String fileNamePrefix) {
-        
-/*		
- * 		We can set any date time format we want.
- *     	For the legend on different formats check http://java.sun.com/j2se/1.4.2/docs/api/java/text/SimpleDateFormat.html
-*/    	
-/*		TODO Make sure that 2 files with same filename are not generated. 
- * 		Current granularity is set at till milliseconds.
-*/
-    	
-        String dateFormat = "MM-dd-yyyy-hh-mm-a-SSS";
-        Calendar currentTemporalSnapshot = Calendar.getInstance();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
-        String timestamp = "-" + simpleDateFormat.format(currentTemporalSnapshot.getTime());
-        
-        return fileNamePrefix + timestamp; 
-    }
+	// Put logs in "User working directory"/logs
+	private static final String LOG_DIRECTORY = System.getProperty("user.dir")
+			+ File.separator + "logs";
+	private static final String LOG_PREFIX = "cishell-user";
+
+	private Logger logger;
+
+	/**
+	 * 
+	 * @throws LogToFileCreationException
+	 *             if there is a problem creating the LogToFile
+	 */
+	public LogToFile() throws LogToFileCreationException {
+		try {
+			// Create an appending file handler
+			validateDirectory(LOG_DIRECTORY);
+
+			// For more pattern info, see
+			// http://docs.oracle.com/javase/6/docs/api/java/util/logging/FileHandler.html
+			String logPattern = LOG_DIRECTORY + File.separator + LOG_PREFIX
+					+ "-" + getTimestamp() + ".%u.%g.log";
+
+			FileHandler handler = new FileHandler(logPattern, LIMIT,
+					MAX_NUM_LOG_FILES, APPEND);
+
+			handler.setFormatter(new SimpleFormatter());
+
+			this.logger = Logger.getLogger(LOGGER_NAME);
+			this.logger.addHandler(handler);
+			this.logger.setUseParentHandlers(SEND_TO_PARENT_LOGGER);
+		} catch (InvalidDirectoryException e) {
+			throw new LogToFileCreationException(
+					"The LogToFile logger could not be created.", e);
+		} catch (SecurityException e) {
+			throw new LogToFileCreationException(
+					"The LogToFile logger could not be created.", e);
+		} catch (IOException e) {
+			throw new LogToFileCreationException(
+					"The LogToFile logger could not be created.", e);
+		}
+	}
+
+	/**
+	 * Represents the failure to instantiate LogToFile.
+	 * 
+	 * @author David M. Coe (david.coe@gmail.com)
+	 * 
+	 */
+	public static class LogToFileCreationException extends Exception {
+		private static final long serialVersionUID = -5837126430378342519L;
+
+		public LogToFileCreationException(String message, Throwable cause) {
+			super(message, cause);
+		}
+	}
+
+	/**
+	 * Creates or verifies that a directory exists at the
+	 * {@code directoryPathname} location.
+	 * 
+	 * @param directoryPathname
+	 *            the pathname of the directory to be created/tested
+	 * @throws InvalidDirectoryException
+	 *             If a directory does not exist and cannot be created at the
+	 *             {@code directoryPathname} location.
+	 */
+	private static void validateDirectory(String directoryPathname)
+			throws InvalidDirectoryException {
+		File file = new File(directoryPathname);
+		if (file.exists()) {
+			if (file.isDirectory()) {
+				return;
+			}
+
+			throw new InvalidDirectoryException(
+					"There is a file, not a directory at '" + directoryPathname
+							+ "'.");
+		}
+
+		if (!file.mkdirs()) {
+			throw new InvalidDirectoryException(
+					"The file directory did not exist, but could not be made at '"
+							+ directoryPathname + "'.");
+		}
+	}
+
+	/**
+	 * Represents a failure to validate the directory.
+	 * 
+	 * @author David M. Coe (david.coe@gmail.com)
+	 * 
+	 */
+	private static class InvalidDirectoryException extends Exception {
+		private static final long serialVersionUID = 6314769551833577561L;
+
+		protected InvalidDirectoryException(String message) {
+			super(message);
+		}
+	}
+
+	@Override
+	public void logged(LogEntry entry) {
+		String message = entry.getMessage();
+		if (!logMessage(message)) {
+			return;
+		}
+		
+		Level legel;
+
+		switch (entry.getLevel()) {
+			case LogService.LOG_DEBUG:
+				legel = Level.FINEST;
+				break;
+			case LogService.LOG_ERROR:
+				legel = Level.SEVERE;
+				break;
+			case LogService.LOG_INFO:
+				legel = Level.INFO;
+				break;
+			case LogService.LOG_WARNING:
+				legel = Level.WARNING;
+				break;
+			default:
+				legel = Level.SEVERE;
+				break;
+		}
+
+		this.logger.log(legel, "[" + entry.getBundle().getSymbolicName() + "] " + message + NEWLINE, entry.getException());
+	}
+
+	/**
+	 * If a message begins with one of these prefixes, it should not be logged.
+	 */
+	private static final String[] NON_LOGGED_MESSAGE_PREFIXES = new String[] {
+			"ServiceEvent ", "BundleEvent ", "FrameworkEvent " };
+
+	/**
+	 * Determine if the {@code message} should be logged.
+	 * 
+	 * @param message
+	 * @return {@code true} if the message should be logged, {@code false} if it
+	 *         should not be logged.
+	 */
+	private static boolean logMessage(String message) {
+		if (message == null) {
+			return false;
+		}
+		for (String messagePrefix : NON_LOGGED_MESSAGE_PREFIXES) {
+			if (message.startsWith(messagePrefix)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private static String getTimestamp() {
+		/*
+		 * We can set any date time format we want. For the legend on different
+		 * formats check
+		 * http://java.sun.com/j2se/1.4.2/docs/api/java/text/SimpleDateFormat
+		 * .html
+		 */
+
+		String dateFormat = "MM-dd-yyyy-hh-mm-a";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+		String timestamp = simpleDateFormat.format(Calendar.getInstance()
+				.getTime());
+
+		return timestamp;
+	}
 
 }
