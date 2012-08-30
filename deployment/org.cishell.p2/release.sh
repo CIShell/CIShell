586e8ed0f0a6d42fe037a1ce1b36d33cadf8d82d
dmcoe <dmcoe@indiana.edu>
2012-08-30 16:26:41 -0400

    We're not doing stable yet.  I'm an idiot

--- deployment/org.cishell.p2/release.sh
+++ deployment/org.cishell.p2/release.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 set -e
 
-USAGE="Usage: $0 release_version [--stable]"
-if [[ $# < 1 || $# > 2 ]]
+USAGE="Usage: $0 release_version"
+if [[ $# != 1 ]]
 then 
     echo "$USAGE"
     exit 1
 fi
 
 VERSION=$1
-
-if [[ "$2" ]]
-then
-    if [[ "$2" != "--stable" ]]
-    then
-	echo "$USAGE"
-	exit 1
-    fi
-fi
 
 VERSION_REGEX="^[0-9]+\.[0-9]+\.[0-9]+$"
 if [[ ! "$VERSION" =~ $VERSION_REGEX ]];
@@ -39,14 +30,3 @@ mkdir -p $DEST_BASE
 mkdir "$DEST"
 cp -R $SOURCE -t "$DEST"
 echo "Copied the files from $SOURCE to $DEST"
-
-if [[ "$2" ]]
-then
-    if [[ -e "$DEST_BASE/stable" ]]
-    then
-        rm "$DEST_BASE/stable"
-    fi
-    
-    ln -s "$DEST" "$DEST_BASE/stable"
-    echo "Stable build link updated"
-fi
