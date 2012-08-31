8f75e28ab49ab3433f485da5364a42db1d637310
dmcoe <dmcoe@indiana.edu>
2012-08-31 12:17:35 -0400

    special case dev to remove the directory

--- deployment/org.cishell.p2/release.sh
+++ deployment/org.cishell.p2/release.sh
@@ -7,23 +7,23 @@ if [[ $# != 1 ]]; then
     exit 1
 fi
 
-VERSION=$1
-
-VERSION_REGEX="^[0-9]+\.[0-9]+\.[0-9]+$"
-if [[ ! "$VERSION" =~ $VERSION_REGEX ]]; then
-    echo "Normal release versions look like 1.0.13"
-    read -e -p "Are you sure '$VERSION' is what you want? (y/n):" REPLY
-
-    if [[ "$REPLY" != "y" ]]; then
-		exit 1
-    fi
-fi
 
 SOURCE="./target/repository/*"
 DEST_BASE="/projects/cishell/www/htdocs/p2"
 DEST="$DEST_BASE/$VERSION/"
 
-mkdir -p $DEST_BASE
+VERSION=$1
+VERSION_REGEX="^[0-9]+\.[0-9]+\.[0-9]+$"
+if [[ ! "$VERSION" =~ $VERSION_REGEX ]]; then
+    if [[ "$VERSION" == "dev" ]]; then
+    	rm -r "$DEST"
+    else
+    	exit 1
+    fi
+fi
+
+
+mkdir -p "$DEST_BASE"
 mkdir "$DEST"
-cp -R $SOURCE -t "$DEST"
+cp -R "$SOURCE" -t "$DEST"
 echo "Copied the files from $SOURCE to $DEST"
