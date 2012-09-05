00536b48bb091ac883215cdd8ba5576d7f24938f
Thomas Smith <thomathom@gmail.com>
2012-09-05 16:23:29 -0400

    Think I fixed release.sh finally.
    Need a testing setup for this bad boy!

--- deployment/org.cishell.p2/release.sh
+++ deployment/org.cishell.p2/release.sh
@@ -9,26 +9,31 @@ fi
 
 VERSION=$1
 
-SOURCE="deployment/org.cishell.p2/target/repository/*"
+SOURCE="deployment/org.cishell.p2/target/repository"
 DEST_BASE="/projects/cishell/www/htdocs/p2"
 DEST="$DEST_BASE/$VERSION/"
 
 VERSION_REGEX="^[0-9]+\.[0-9]+\.[0-9]+$"
 if [[ ! "$VERSION" =~ $VERSION_REGEX ]]; then
     if [[ "$VERSION" == "dev" ]]; then
-    	if [ -d "$DEST" ]; then 
+    	if [ -d "$DEST" ]; then
+    		echo "Version is dev: removing already-existing dev p2 repo"
     		rm -r "$DEST"
     	fi
     else
-    	exit 1
+    	# exit codes: see http://tldp.org/LDP/abs/html/exitcodes.html#EXITCODESREF
+    	echo "Fatal: Bad version number (not 0.0.0 or dev)"
+    	exit 64
     fi
 fi
 
 echo "Going to copy $SOURCE to $DEST"
-pwd
-ls -l "$SOURCE"
+
+if [ -d "$DEST" ]; then
+	echo "Fatal: $DEST already exists (and version is not dev)"
+	exit 65
+fi
 
 mkdir -p "$DEST_BASE"
-mkdir "$DEST"
-cp -R "$SOURCE" -t "$DEST"
+cp -R "$SOURCE" "$DEST"
 echo "Copied the files from $SOURCE to $DEST"
